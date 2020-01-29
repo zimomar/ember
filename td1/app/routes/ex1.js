c7df3a3d253303e870dbe2ee09b27e746169b2c6
ABOUSLIME Omar <21800354@etu.unicaen.fr>
2020-01-29 12:02:50 +0100

    manque maxlength

--- td1/app/routes/ex1.js
+++ td1/app/routes/ex1.js
@@ -2,7 +2,19 @@ import Route from '@ember/routing/route';
 import Note from 'td1/utils/note-class';
 
 export default Route.extend({
+
   model(){
     return Note.create();
   }
+  ,
+  actions:{
+    save(note){
+      note.set("info","La note a bien été sauvegardée");
+    },
+    clear(note){
+      note.set("info","");
+    }
+
+  }
+
 });
--- td1/app/utils/note-class.js
+++ td1/app/utils/note-class.js
@@ -3,4 +3,21 @@ import EmberObject, { computed } from '@ember/object';
 export default EmberObject.extend({
   MAX:100,
   content:'Entrez votre texte',
+  info:'',
+  size:computed("content",function(){
+    return this.get('MAX')-this.get('content').length;
+  }),
+  style:computed("size",function(){
+    if(this.size>50){
+      return "alert alert-success";
+    } else if(this.size<=50 && this.size>=20){
+      return "alert alert-warning";
+    } else {
+      return "alert alert-danger";
+    }
+  }),
+
+  alertVisible:computed("info",function(){
+    return this.get("info").length!=0;
+  })
 });
